a9f8fdbdddb59f865362865a00830ad59aad7c2f
Feliciano  MANDJAM <felicianomandja@gmail.com>
2024-04-22 16:39:46 +0200

    First commit

--- Gestion-de-taches-TypeScript/front-gestion-de-taches/src/App.tsx
+++ Gestion-de-taches-TypeScript/front-gestion-de-taches/src/App.tsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
+import Home from './pages/Home/Home'
+import Sign from './pages/Auth/Sign'
+import SignUp from './pages/Auth/SignUp'
+import {BrowserRouter , Routes , Route} from "react-router-dom"
+import Header from "./utils/componants/Header.jsx"
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-     <h1>Page app</h1>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route index path='/' element={<Home />}/>
+        <Route path='/signin' element={<Sign />}/>
+        <Route path='/signup' element={<SignUp />}/>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- Gestion-de-taches-TypeScript/front-gestion-de-taches/src/main.tsx
+++ Gestion-de-taches-TypeScript/front-gestion-de-taches/src/main.tsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Provider - Redux
+// import { Provider } from "react-redux"
+import { AuthProvider } from './utils/context/AuthContext.tsx'
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <AuthProvider>
+    {/* <Provider store={store}/> */}
     <App />
+    </AuthProvider>
   </React.StrictMode>,
 )
